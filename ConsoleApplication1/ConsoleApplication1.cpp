efde0101a41ba9f487034a740e8d0fbc0d4c0103
MalX_qp59@student.itstep.org <MalX_qp59@student.itstep.org>
2022-02-25 16:15:38 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,54 @@
+﻿#include<iostream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+void f(int* a, int n, int k)
+{
+    int i, j, temp;
+    switch (k)
+    {
+    case 1:
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n - 1; j++)
+                if (a[j] < a[j + 1])
+                {
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+        }break;
+    case 2:
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n - 1; j++)
+                if (a[j] > a[j + 1])
+                {
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+        }break;
+    }
+
+
+
+}
+
+int main() {
+    int* a, i, k, n;
+    cout << "Vvedite kolichestvo" << endl;
+    cin >> n;
+    a = new int[n];
+
+    cout << "Vvedite elementi" << endl;
+    for (i = 0; i < n; i++)
+        cin >> a[i]; cin >> k;
+    f(a, n, k);
+    for (i = 0; i < n; i++)
+        cout << a[i] << setw(5);
+
+
+    delete[]a;
+    return 0;
+}
